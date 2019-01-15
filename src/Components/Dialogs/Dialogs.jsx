a26600db21ef953c543097cd21d6b30f25424df0
safronman <safronmanbox@gmail.com>
2019-01-15 09:47:48 +0300

    Temp recision. Add data arrays

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -3,21 +3,39 @@ import s from './Dialogs.module.css';
 import DialogPreview from "./DialogPreview/DialogPreview";
 import DialogMainContent from "./DialogMainContent/DialogMainContent";
 
+let dialogsData = [
+    {id: 1, name: "James"},
+    {id: 2, name: "Bill"},
+    {id: 3, name: "David"}
+];
+
+let messagesData = [
+    {id: 1, message: "Good morning"},
+    {id: 2, message: "Hello"},
+    {id: 3, message: "How are you?"},
+    {id: 4, message: "I'm fine, thanks"},
+
+];
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsPeople}>
                 <ul className={s.dialogsList}>
-                    <DialogPreview pathId="1" name="James"/>
-                    <DialogPreview pathId="2" name="Bill"/>
-                    <DialogPreview pathId="3" name="David"/>
+                    {
+                        dialogsData.map((item) => {
+                            return <DialogPreview id={item.id} name={item.name}/>
+                        })
+                    }
                 </ul>
             </div>
 
             <div className={s.dialogsListContent}>
-                <DialogMainContent message="Hello my friend"/>
-                <DialogMainContent message="How are you?"/>
-                <DialogMainContent message="London is the capital of Great Britain"/>
+                <DialogMainContent message={messagesData[0].message}/>
+                <DialogMainContent message={messagesData[1].message}/>
+                <DialogMainContent message={messagesData[2].message}/>
+                <DialogMainContent message={messagesData[3].message}/>
+
             </div>
         </div>
     );
