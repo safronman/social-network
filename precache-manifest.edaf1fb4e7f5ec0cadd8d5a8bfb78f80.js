b7f7a2cd9ba4f10e2519b0d711584c86c4e318d7
safronman <safronmanbox@gmail.com>
2019-03-07 09:02:58 +0300

    Updates

---
+++ precache-manifest.edaf1fb4e7f5ec0cadd8d5a8bfb78f80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f27e091c9f796bcd7ce86d7d2c8aa8c3",
+    "url": "/projects/social-network/static/media/avatar-1.f27e091c.svg"
+  },
+  {
+    "revision": "cfdd03582a0becc7012d",
+    "url": "/projects/social-network/static/css/main.65297f64.chunk.css"
+  },
+  {
+    "revision": "3627863f1e3b40480a9b",
+    "url": "/projects/social-network/static/js/runtime~main.e947bfa3.js"
+  },
+  {
+    "revision": "aeacebfd35138127c77b",
+    "url": "/projects/social-network/static/js/2.95a4a526.chunk.js"
+  },
+  {
+    "revision": "c5812e4f67ea1732bbea69549c8de2f5",
+    "url": "/projects/social-network/static/media/avatar.c5812e4f.png"
+  },
+  {
+    "revision": "cfdd03582a0becc7012d",
+    "url": "/projects/social-network/static/js/main.23c42965.chunk.js"
+  },
+  {
+    "revision": "e06e0dcfb2169fd273a4d2b71d546e84",
+    "url": "/projects/social-network/static/media/logo.e06e0dcf.svg"
+  },
+  {
+    "revision": "ba83dc05d828b4c88a3363a8b233df99",
+    "url": "/projects/social-network/static/media/cover.ba83dc05.jpg"
+  },
+  {
+    "revision": "8671940032d781cfeb92a0ebf86bdd67",
+    "url": "/projects/social-network/static/media/avatar-2.86719400.svg"
+  },
+  {
+    "revision": "22f9e6a1e4bbcb1fe5eaf261c52bb943",
+    "url": "/projects/social-network/static/media/avatar-3.22f9e6a1.svg"
+  },
+  {
+    "revision": "f5d65001c978f78679c3d25d617218b3",
+    "url": "/projects/social-network/index.html"
+  }
+];
