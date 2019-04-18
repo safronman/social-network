38624ad2448d7f638933f7b7fd980c4d5769d45d
safronman <safronmanbox@gmail.com>
2019-04-18 14:02:00 +0300

    refactored UnreadMessagesCount component

--- src/components/UnreadMessagesCount/UnreadMesagesCount.js
+++ src/components/UnreadMessagesCount/UnreadMesagesCount.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './UnreadMesagesCount.module.css'
+import PropTypes from "prop-types";
+import Dialogs from "../Dialogs/Dialogs";
 
 const UnreadMessagesCount = (props) => {
     return (
         <span className={styles.counter}>{props.unreadMessagesCounter}</span>
     );
+};
+
+Dialogs.propTypes = {
+    unreadMessagesCounter: PropTypes.number
 };
 
 export default UnreadMessagesCount;
