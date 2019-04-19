c30b50a246da21a4cb16243d572b8d6cb9e8e69d
safronman <safronmanbox@gmail.com>
2019-04-19 10:05:38 +0300

    refactored ProfilePhoto styles

--- src/components/Profile/ProfilePhoto/ProfilePhoto.js
+++ src/components/Profile/ProfilePhoto/ProfilePhoto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './ProfilePhoto.module.css';
+import styles from './../Profile.module.css';
 
 const ProfilePhoto = (props) => {
 
@@ -8,8 +8,8 @@ const ProfilePhoto = (props) => {
     };
 
     return (
-        <div className={styles.profilePhoto_wrapper}>
-            <img className={styles.profilePhoto_img} src={props.avatar ? props.avatar :'https://www.fillmurray.com/100/100'} alt="avatar"/>
+        <div className={styles.profilePhotoWrapper}>
+            <img className={styles.profilePhotoImg} src={props.avatar ? props.avatar :'https://www.fillmurray.com/100/100'} alt="avatar"/>
             <input type="file"
                    id='ownerAvatar'
                    onChange={onPhotoChange}/>
