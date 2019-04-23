8ccf10162f5918172bfdf745ca5b0611c0530d98
safronman <safronmanbox@gmail.com>
2019-04-23 13:04:20 +0300

    add redirectToLoginPage

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './NotFound.module.css';
+import {NavLink} from "react-router-dom";
 
 const NotFound = () => {
     return (
         <>
             <h2 className={styles.title}>404</h2>
             <p className={styles.desc}>page not found</p>
+            <NavLink to='/' className={styles.redirectToLoginPage}>Перейдите на страницу логина</NavLink>
         </>
     );
 };
