b4969df32510103aaada270b4d59c18cf685fb93
safronman <safronmanbox@gmail.com>
2019-04-24 08:29:38 +0300

    add header

--- src/dal/axiosInstance.js
+++ src/dal/axiosInstance.js
@@ -3,9 +3,9 @@ import * as axios from "axios";
 const axiosInstance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
-    // headers: {
-    //     'API_KEY': 'e394c5d5-ee32-4ec0-bd66-cc95dd95ee43'
-    // }
+    headers: {
+        'API-KEY': 'e394c5d5-ee32-4ec0-bd66-cc95dd95ee43'
+    }
 });
 
 export default axiosInstance;
